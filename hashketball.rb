653b4d8773da0367a955610ba82deb72af10ed23
Anthony Pardo <pardo.anthony@gmail.com>
2019-11-12 22:43:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -219,17 +219,11 @@ def most_points_scored()
 end
 
 def winning_team()
-  max_points = 0 
-  player_with_max = ""
-  game_hash.each do |team, attribute|
-    attribute[:players].each do |player|
-      if player[:points] > max_points
-        max_points = player[:points]
-        player_with_max = player[:player_name]
-      end
-    end
+  nets_points = 0 
+  hornets_points = 0 
+  game_hash[:home][:players].each do |player|
+    nets_points += player[:points]
   end
-  player_with_max
 end
 
 
